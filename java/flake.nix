9dcd76e751840486d071d6c9addcfc927dc4d470
Free-Rat <lawicki02@gmail.com>
2024-03-14 18:05:09 +0100

    add java shell

---
+++ java/flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Flake with shell for Java dev";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.x86_64-linux.default =
+        pkgs.mkShell {
+
+          shellHook = ''
+            echo "nix develop"
+          '';
+
+          packages = with pkgs; [
+            maven
+			gradle
+            # jdk
+            git
+			jetbrains.idea-community
+			jetbrains.jdk
+          ];
+        };
+    };
+}
